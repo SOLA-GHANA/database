97a848df09e4dcbe54ab3c63665a31390103ec06
Kwesi Smith <nanakwesi@gmail.com>
2012-05-21 14:35:24 +0000

    Created database_create_GHANA.sh
    This file is customized to load Ghana's spatial data.

---
+++ database_create_GHANA.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+host="localhost"
+#host="exlprcadreg3.ext.fao.org"
+script_folder="./"
+dbname="sola"
+nopass=""
+#psql --host=$host --port=5432 --username=postgres $nopass --dbname=$dbname --command="create database "$dbname" with encoding='UTF8' template=template_postgis connection limit=-1;"
+psql --host=$host --port=5432 --username=postgres $nopass --dbname=$dbname --file=$script_folder"sola.sql"
+psql --host=$host --port=5432 --username=postgres $nopass --dbname=$dbname --file=$script_folder"test_data.sql"
+psql --host=$host --port=5432 --username=postgres $nopass --dbname=$dbname --file=$script_folder"business_rules.sql"
+psql --host=$host --port=5432 --username=postgres $nopass --dbname=$dbname --file=$script_folder"test-data/ghana/migrate-spatial.sql"
+
