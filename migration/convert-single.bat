8ca97ed8070d8a969969911c17aa2f471d1374a3
Elton Manoku <elton.manoku@fao.org>
2012-06-01 21:04:27 +0200

    Modify migration script and add a document
    The migration script is reorganized. Also it is added a document that
    explains the steps to convert data from arcinfo format to database tables.
    A readme file is added also.

--- migration/convert-single.bat
+++ migration/convert-single.bat
@@ -8,13 +8,13 @@ set region=%6
 set srid=2136
 set extra_options=--quiet -v ON_ERROR_STOP=1 
 rd /s /q data\dest\%sectionnr%
-7z -y x data\source\%sectionnr%.ZIP -odata\dest\%sectionnr%
-avcimport data\dest\%sectionnr%\LOT.E00 data\dest\%sectionnr%\cov_lot
-avcimport data\dest\%sectionnr%\BLOCK.E00 data\dest\%sectionnr%\cov_block
-ogr2ogr -f "ESRI Shapefile" -skipfailures -overwrite data\dest\%sectionnr%\shape_lot data\dest\%sectionnr%\cov_lot
-ogr2ogr -f "ESRI Shapefile" -skipfailures -overwrite data\dest\%sectionnr%\shape_block data\dest\%sectionnr%\cov_block
-psql\shp2pgsql -a -s %srid% data\dest\%sectionnr%\shape_block\PAL testdata.shape_block > data\dest\tmp.sql
-psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --file=data\dest\tmp.sql
-psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --command="update testdata.shape_block set section='%sectionnr%', region='%region%' where section is null"
-psql\shp2pgsql -a -s %srid% data\dest\%sectionnr%\shape_lot\PAL testdata.shape_lot > data\dest\tmp.sql
-psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --file=data\dest\tmp.sql
+tools\7z -y x data\source\%sectionnr%.ZIP -odata\dest\%sectionnr%
+tools\avcimport data\dest\%sectionnr%\LOT.E00 data\dest\%sectionnr%\cov_lot
+tools\avcimport data\dest\%sectionnr%\BLOCK.E00 data\dest\%sectionnr%\cov_block
+tools\ogr2ogr -f "ESRI Shapefile" -skipfailures -overwrite data\dest\%sectionnr%\shape_lot data\dest\%sectionnr%\cov_lot
+tools\ogr2ogr -f "ESRI Shapefile" -skipfailures -overwrite data\dest\%sectionnr%\shape_block data\dest\%sectionnr%\cov_block
+tools\psql\shp2pgsql -a -s %srid% data\dest\%sectionnr%\shape_block\PAL staging_area.shape_block > data\dest\tmp.sql
+tools\psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --file=data\dest\tmp.sql
+tools\psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --command="update staging_area.shape_block set section='%sectionnr%', region='%region%' where section is null"
+tools\psql\shp2pgsql -a -s %srid% data\dest\%sectionnr%\shape_lot\PAL staging_area.shape_lot > data\dest\tmp.sql
+tools\psql\psql %extra_options% --host=%db_host% --port=%db_port% --username=%db_user% --dbname=%db_name% --file=data\dest\tmp.sql
