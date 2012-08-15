8b97a7d399d43a054e2dfdc3a27c7c608ce9050a
Elton Manoku <elton.manoku@fao.org>
2012-08-15 14:13:08 +0200

    Add changes to clients and services for ghana

--- migration/db-scripts/migrate-spatial.sql
+++ migration/db-scripts/migrate-spatial.sql
@@ -1,16 +1,24 @@
 ﻿--Create handy views
+
+--View that gives the districts
+drop view if exists staging_area.district_source cascade;
+create or replace view staging_area.district_source as 
+select region as region_id, distric_nr as num, 'District ' || distric_nr as locality, 
+0 as year_declared, st_geometryn(the_geom,1) as the_geom 
+from staging_area.d03;
+
 --View that gives the parcels
 create or replace view staging_area.parcel as 
 select 
-b.region || '/' || substring(b.section  from 2 for 3) || '/' || b.blockno || '/' || l.lotno as id, 
+b.region || '/' || d.num || '/' || substring(b.section  from 2 for 3) || '/' || b.blockno || '/' || l.lotno as id, 
 'parcel' as type_code,
-b.region || '/' || substring(b.section  from 2 for 3) || '/' || b.blockno as name_firstpart,
+b.region || '/' || d.num || '/' || substring(b.section  from 2 for 3) || '/' || b.blockno as name_firstpart,
 l.lotno as name_lastpart,
 'current' as status_code,
 st_transform(st_geometryn(l.geom,1), 32630) as geom_polygon,
 'migration-transaction' as transaction_id
-from staging_area.shape_lot l, staging_area.shape_block b
-where l.lotno != '9999' and l.geom && b.geom and st_intersects(st_centroid(l.geom), b.geom);
+from staging_area.shape_lot l, staging_area.shape_block b, staging_area.district_source d
+where l.lotno != '9999' and l.geom && b.geom and st_intersects(st_centroid(l.geom), b.geom) and st_intersects(st_centroid(l.geom), d.the_geom) ;
 
 -- Create a view that is used to retrieve duplications
 drop view if exists staging_area.parcel_duplications;
@@ -22,7 +30,10 @@ having count(*)>1
 order by 1,2;
 
 --Empty the table where the data about regions, districts, rections and blocks are stored
-delete from cadastre.spatial_unit_group;
+delete from cadastre.block;
+delete from cadastre.section;
+delete from cadastre.district;
+delete from cadastre.region;
 --Empty the table where the data about parcels are stored
 delete from cadastre.spatial_unit;
 --Empty the transaction table
@@ -32,25 +43,24 @@ delete from transaction.transaction;
 insert into transaction.transaction(id, status_code, approval_datetime) values('migration-transaction', 'approved', now());
 
 -- Insert regions. Regions have hierarchy 1
-insert into cadastre.spatial_unit_group(id, hierarchy_level, label, name, geom)
+insert into cadastre.region(id, code, name, the_geom)
 select 
-code as id, 
-1 as hierarchy_level, 
-name as label, 
-name as name, 
-st_multi(st_transform(the_geom,32630)) as geom 
+case when code = 'AA' then 'GA' else code end as id, 
+case when code = 'AA' then 'GA' else code end,
+name, 
+st_transform(the_geom,32630) as the_geom 
 from staging_area.region
 where the_geom is not null and st_isvalid(the_geom);
 
 -- Insert districts. Districts have hierarchy 2
-insert into cadastre.spatial_unit_group(id, hierarchy_level, label, name, geom)
+insert into cadastre.district(id, region_id, num, year_declared, the_geom)
 select 
-region || '/' || district_nr as id, 
-2 as hierarchy_level, 
-district_nr as label, 
-region || '/' || district_nr as name, 
-st_multi(st_transform(the_geom,32630)) as geom
-from staging_area.district
+region_id || '/' || num as id, 
+region_id, 
+num, 
+year_declared, 
+st_transform(the_geom,32630) as the_geom
+from staging_area.district_source
 where the_geom is not null and st_isvalid(the_geom);
 
 -- Insert sections. Sections have hierarchy 3
